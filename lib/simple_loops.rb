82c04c29d0cb460195af1386f4fa5a6e27574cab
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 05:24:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World."
+
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, number)
+  message = ["Hello Moon.", "Hello Red Balloon"]
+  number = [5, 10]
+
+  while number[0] do
+    puts message[0]
+  end
+  while number[1] do
+    puts message[1]
+  end
+end

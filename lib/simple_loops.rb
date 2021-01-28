8f08006bb7ec796afbcd8bc37123f9c17126b906
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 05:28:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,11 +10,9 @@ end
 def loop_message_n_times(message)
   message = ["Hello Moon.", "Hello Red Balloon"]
 
-  while message[0] do
     5.times do
       puts message[0]
   end
-  while number[1] do
     10.times do
       puts message[1]
   end
